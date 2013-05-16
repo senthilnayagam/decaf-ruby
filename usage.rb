25d89d7411ffa8429853b2844369eacec7d2046f
Senthil Nayagam <senthil@railsfactory.com>
2013-05-16 14:08:06 +0530

    usage instructions

---
+++ usage.rb
@@ -0,0 +1,20 @@
+
+=begin
+
+run this commands inside your decaf ruby shell
+
+
+=end
+
+
+# window.open("www.youraddress.com","_self")
+
+location.href  = "https://raw.github.com/senthilnayagam/decaf-ruby/master/index.html"
+
+
+d =document.create_element "script"
+d.type = 'text/ruby'
+d.src = 'https://raw.github.com/senthilnayagam/decaf-ruby/master/hello.rb'
+
+
+doc = document.get_elements_by_tag_name('head').appendChild(script)
