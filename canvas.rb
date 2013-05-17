cdbedf4d8a552d6503a602d96d28f905d48a2025
Senthil Nayagam <senthil@railsfactory.org>
2013-05-17 11:18:58 +0530

    drawing lines, arc, text, gradients

--- canvas.rb
+++ canvas.rb
@@ -29,5 +29,43 @@ ctx.fill_style = "#FF0000"
 # "#FF0000"
 ctx.fill_rect(0,0,150,75)
 #null
-
 # a small rectangle now must be visble on your browser
+
+# draw a line
+ctx.move_to(0,0);
+ctx.line_to(200,100);
+ctx.stroke();
+
+
+
+ctx.begin_path();
+ctx.arc(95,50,40,0,2*Math::PI);
+ctx.stroke();
+
+
+ctx.font="30px Arial";
+ctx.fill_text("Hello World",10,50);
+
+ctx.stroke_text("Hello World",10,50);
+
+
+#Create gradient
+grd=ctx.create_linear_gradient(0,0,200,0);
+grd.add_color_stop(0,"red");
+grd.add_color_stop(1,"white");
+
+
+#Fill with gradient
+ctx.fill_style=grd;
+ctx.fill_rect(10,10,150,80);
+
+
+
+# Create gradient
+grd2=ctx.create_radial_gradient(75,50,5,90,60,100);
+grd2.add_color_stop(0,"red");
+grd2.add_color_stop(1,"white");
+
+# Fill with gradient
+ctx.fill_style=grd2;
+ctx.fill_rect(10,10,150,80);
